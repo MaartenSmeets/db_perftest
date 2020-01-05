dd033924bc831fc71258c2614dff9bff82d76fec
Maarten <maarten@localhost.localdomain>
2020-01-05 14:54:25 +0100

    Removing

--- test_apps/dbapp_quarkus_hibernate/src/test/java/nl/amis/smeetsm/NativeDbAppResourceIT.java
+++
@@ -1,9 +0,0 @@
-package nl.amis.smeetsm;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeDbAppResourceIT extends DbAppResourceTest {
-
-    // Execute the same tests but in native mode.
-}
