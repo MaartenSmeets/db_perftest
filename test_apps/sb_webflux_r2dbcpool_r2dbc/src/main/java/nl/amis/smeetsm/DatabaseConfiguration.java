d9d05e9e92123ab1c2a11eaef5e9953154117615
Maarten <maarten@localhost.localdomain>
2020-01-10 16:38:14 +0100

    dding new reactive sample

---
+++ test_apps/sb_webflux_r2dbcpool_r2dbc/src/main/java/nl/amis/smeetsm/DatabaseConfiguration.java
@@ -0,0 +1,72 @@
+package nl.amis.smeetsm;
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+import static io.r2dbc.pool.PoolingConnectionFactoryProvider.MAX_SIZE;
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
+//update with https://github.com/Aleksandr-Filichkin/r2dbc-vs-jdbc/blob/master/r2dbc-service/src/main/java/com/filichkin/blog/db/reactive/SubscriptionConfiguration.java
+// https://medium.com/@filia.aleks/r2dbc-vs-jdbc-19ac3c99fafa
+@Configuration
+@EnableR2dbcRepositories
+class DatabaseConfiguration extends AbstractR2dbcConfiguration {
+
+    @Value("${spring.r2dbc.host}")
+    String host;
+
+    @Value("${spring.r2dbc.port}")
+    Integer port;
+
+    @Value("${spring.r2dbc.username}")
+    String username;
+
+    @Value("${spring.r2dbc.password}")
+    String password;
+
+    @Value("${spring.r2dbc.database}")
+    String database;
+
+    private static final String DB_PROTOCOL = "postgresql";
+    private static final String DB_DRIVER = "pool";
+    private int maxClientConnections = 1000;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+                .option(DRIVER, DB_DRIVER)
+                .option(PROTOCOL, DB_PROTOCOL)
+                .option(MAX_SIZE, maxClientConnections)
+                .option(HOST, host)
+                .option(PORT, port)
+                .option(USER, username)
+                .option(PASSWORD, password)
+                .option(DATABASE, database)
+                .build());
+        //unlimited connections below?
+        //return new PostgresqlConnectionFactory(
+        //        PostgresqlConnectionConfiguration.builder()
+        //                .host(host)
+        //                .port(port)
+        //                .database(database)
+        //                .username(username)
+        //                .password(password)
+        //                .build()
+        //);
+    }
+
+    @Bean
+    ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
+    }
+}
