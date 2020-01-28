dd37eae6f1348ac10d12532cfe44093476e81df1
Maarten <wisp1980@gmail.com>
2020-01-28 15:04:25 +0100

    Updating sample code DB initialization

--- test_apps/sb_webflux_r2dbcpool_r2dbc/src/main/java/nl/amis/smeetsm/DatabaseConfiguration.java
+++ test_apps/sb_webflux_r2dbcpool_r2dbc/src/main/java/nl/amis/smeetsm/DatabaseConfiguration.java
@@ -8,8 +8,12 @@ import io.r2dbc.spi.ConnectionFactoryOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
+import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
+import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.transaction.ReactiveTransactionManager;
 
@@ -68,5 +72,18 @@ class DatabaseConfiguration extends AbstractR2dbcConfiguration {
     @Bean
     ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
         return new R2dbcTransactionManager(connectionFactory);
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
     }
 }
--- test_apps/sb_webflux_r2dbcpool_r2dbc/src/main/resources/schema.sql
+++ test_apps/sb_webflux_r2dbcpool_r2dbc/src/main/resources/schema.sql
@@ -1,14 +1,24 @@
-DROP TABLE public.person;
+DROP TABLE IF EXISTS public.person;
 
 CREATE TABLE public.person
 (
-    id bigint SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name character varying(255) COLLATE pg_catalog."default",
-    last_name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT person_pkey PRIMARY KEY (id)
+    last_name character varying(255) COLLATE pg_catalog."default"
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.person
-    OWNER to postgres;
+ALTER TABLE public.person OWNER to postgres;
+
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+INSERT INTO public.person (first_name,last_name) values ('Maarten','Smeets');
+COMMIT;
