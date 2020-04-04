ef926a350c235dfc49583f9caa4dbf07ae2f5ab5
Maarten <wisp1980@gmail.com>
2020-04-04 11:08:05 +0200

    Focus on R2DBC and updating to latest releases

--- test_apps/jaxrsrxjava_jpa_hikari_jdbc/src/main/java/nl/amis/smeetsm/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package nl.amis.smeetsm;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-}
