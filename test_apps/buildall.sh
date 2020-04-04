6a114d0202dd0f4dda97812aa54bd42eb5edee8f
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2020-04-04 11:10:40 +0200

    Focus on R2DBC

--- test_apps/buildall.sh
+++ test_apps/buildall.sh
@@ -1,10 +1,5 @@
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
-cd jaxrsrxjava_jpa_hikari_jdbc
-mvn clean package
-mv target/*.jar ..
-cd ..
-
 cd sb_jpa_hikari_jdbc
 mvn clean package
 mv target/*.jar ..
@@ -23,14 +18,4 @@ cd ..
 cd sb_webflux_r2dbcpool_r2dbc
 mvn clean package
 mv target/*.jar ..
-cd ..
-
-cd qs_resteasy_r2dbcpool_r2dbc
-mvn clean package
-mv target/qs_resteasy_r2dbcpool_r2dbc-1.0-SNAPSHOT-runner.jar ../qs_resteasy_r2dbcpool_r2dbc-1.0-SNAPSHOT.jar
-cd ..
-
-cd qs_resteasy_agroalpool_jdbc
-mvn clean package
-mv target/qs_resteasy_agroalpool_jdbc-1.0-SNAPSHOT-runner.jar ../qs_resteasy_agroalpool_jdbc-1.0-SNAPSHOT.jar
 cd ..
